e072ac806276d5834be8b4f8d2a26736dc148696
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2021-10-30 19:08:08 -0300

    Inicio de aula sobre condições

---
+++ Aula 08 - Condições/script.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log('A velocidade do seu carro é 60.5')
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Direja sempre com segurança')
+
