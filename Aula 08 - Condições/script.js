f449e8432efcbca496b3fc898e976ea8d17ea8f7
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2021-11-23 22:29:07 -0300

    Condições simples

--- Aula 08 - Condições/script.js
+++ Aula 08 - Condições/script.js
@@ -1,7 +1,10 @@
-var vel = 12
-console.log('A velocidade do seu carro é 60.5')
-if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+var vel = 15
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel >= 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log('Direja sempre com segurança')
+
+console.log('Dirija sempre usando cinto de segurança!')
 
